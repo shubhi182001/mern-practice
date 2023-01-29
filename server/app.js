9daa8bc381bc2cb6e515039ee37cc221f9285ded
shubhi182001 <kediashubhi@gmail.com>
2023-01-29 09:55:10 +0530

    securing

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
+const dotenv = require("dotenv");
 const express = require("express");
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+require('./db/conn');
+
 
 
 //Middleware: whenever we want to authorize and check we make use of middleware. 
-//we first authorize then call next();
+//we first authorize then call next();  
 const middleware = (req, res, next) => {
     console.log("hello from middleware");
     next();
@@ -13,6 +20,7 @@ app.get("/", (req, res) => {
     res.send("Hello from server")
 })
 app.get("/about", middleware, ( req, res) => {
+    console.log("middleware executed first")
     res.send("hello from about");
 })
 app.get("/contact", (req, res) => {
@@ -24,6 +32,10 @@ app.get("/signin", (req, res)=> {
 app.get("/signup", (req, res) => {
     res.send("Hello from signup page");
 })
-app.listen(3000, ()=>{
+
+
+
+
+app.listen(PORT, ()=>{
     console.log("server running at 3000");
 })
