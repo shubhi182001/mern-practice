fb59327aac4bab303eb9aba59fadea83ce4dd1e4
shubhi182001 <kediashubhi@gmail.com>
2023-01-29 19:15:45 +0530

    hashed

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs")
 
 
 const User = require("../model/userSchema");
@@ -21,17 +22,47 @@ router.post("/register", async (req, res) => {
         const userExsist = await User.findOne({email: email});
         if(userExsist){
             return res.status(402).json({error: "USer already exsists"});
+        }else if(password != cpassword){
+            return res.status(402).json({error: "Password not matching"});
+        }else{
+            const user = new User({name, email, phone, work, password, cpassword})
+            //yaha p schema m pre wala method aaega fir neeche execute ho jaega
+            const userRegister = await user.save();
+            if(userRegister){
+                res.status(201).json({message: "user registered successfully"});
+            }
         }
-        const user = new User({name, email, phone, work, password, cpassword})
-        const userRegister = await user.save();
-        if(userRegister){
-            res.status(201).json({message: "user registered successfully"});
-        }
-
     }catch(e){
         console.log(e);
     }
    
 })
+
+
+router.post("/signin", async(req, res) => {
+    try{
+        const {email ,password} = req.body;
+        if(!email || !password){
+            return res.status(401).json({error: "invalid input"});
+        }
+        const userLogin = await User.findOne({email: email});
+        if(userLogin){
+            const isMatch = await bcrypt.compare(password, userLogin.password);
+            if(!isMatch){
+                res.status(402).json({error:"Invalid Credentials"});
+            }
+            else{           
+                res.json({message:"User sign in successfull"});
+            }
+        }else{
+            res.status(402).json({error:"Email does not exsist"});
+        }
+        
+    }catch(e){
+        console.log(e);
+    }
+})
+
+
 
 module.exports = router;
